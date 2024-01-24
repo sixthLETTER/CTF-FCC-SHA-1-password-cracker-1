145e1be22fa1d63678d4d8eb18a4b2ac4320f601
Replit user <>
2024-01-24 15:58:25 +0000

    made some changes in cracker.py

--- password_cracker.py
+++ password_cracker.py
@@ -1,4 +1,35 @@
 import hashlib
 
-def crack_sha1_hash(hash):
+def crack_sha1_hash(hash, use_salts=False):
+    passwords_arr = []
+    read_and_add_to_arr("top-10000-passwords.txt", passwords_arr)
+
+    if use_salts:
+        top_salt_passwords = {}
+        top_salts = []
+        read_and_add_to_arr("known-salts.txt", top_salts)
+        for bsalt in top_salts:
+            for bpassword in passwords_arr:
+                prepended = hashlib.sha1(bsalt + bpassword).hexdigest()
+                appended = hashlib.sha1(bpassword + bsalt).hexdigest()
+                top_salt_passwords[prepended] = bpassword.decode("utf-8")
+                top_salt_passwords[appended] = bpassword.decode("utf-8")
+            if hash in top_salt_passwords:
+                return top_salt_passwords[hash]
+
+    passwords_dict = {}
+    for p in passwords_arr:
+        hash_line = hashlib.sha1(p).hexdigest()
+        passwords_dict[hash_line] = p.decode("utf-8")
+
+    if hash in passwords_dict:
+        return passwords_dict[hash]
+
+    return "PASSWORD NOT IN DATABASE"
     
+def read_and_add_to_arr(file_name, arr):
+    with open(file_name, "rb") as f:
+        line = f.readline().strip()
+        while line:
+            arr.append(line)
+            line = f.readline().strip()
